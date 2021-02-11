719537136d15dd969f37c61d1c697eac263935ef
Cory Spicer <corys4139@gmail.com>
2021-02-11 13:56:43 -0500

    make form less declarative and clean up syntax

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { useFormik } from 'formik';
+import * as Yup from 'yup'
 import './styles.css';
 
-const validate = values => {
-    const errors = {};
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (values.firstName.length > 15){
-        errors.firstName = 'Must be 15 characters or less';
-    }
+// custom validation with Formik:
 
-    if (!values.lastName) {
-        errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less';
-    }
+// const validate = values => {
+//     const errors = {};
+//     if (!values.firstName) {
+//         errors.firstName = 'Required';
+//     } else if (values.firstName.length > 15){
+//         errors.firstName = 'Must be 15 characters or less';
+//     }
 
-    if (!values.email) {
-        errors.email = 'Required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = 'Invalid email address';
-    }
-    return errors
-}
+//     if (!values.lastName) {
+//         errors.lastName = 'Required';
+//     } else if (values.lastName.length > 20) {
+//         errors.lastName = 'Must be 20 characters or less';
+//     }
+
+//     if (!values.email) {
+//         errors.email = 'Required';
+//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+//         errors.email = 'Invalid email address';
+//     }
+//     return errors
+// }
 
 const SignupForm = () => {
     const formik = useFormik({
@@ -32,7 +35,20 @@ const SignupForm = () => {
             firstName: "",
             lastName: ""
         },
-        validate,
+
+        // validation using Yup
+
+        validationSchema: Yup.object({
+            firstName: Yup.string()
+                .max(15, 'Must be 15 characters or less')
+                .required('Required'),
+            lastName: Yup.string() 
+                .max(20, 'Must be 20 characters or less')
+                .required('Required'),
+            email: Yup.string()
+                .email('Invalid email address')
+                .required('Required'),
+        }),
         onSubmit: values => {
             alert(JSON.stringify(values, null, 2));
         }
@@ -42,11 +58,8 @@ const SignupForm = () => {
             <label htmlFor="firstName">First Name</label>
             <input
                 id="firstName"
-                name="firstName"
                 type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.firstName}
+                {...formik.getFieldProps('firstName')}
             />
             {formik.touched.firstName && formik.errors.firstName ? ( 
                 <div>{formik.errors.firstName}</div>
@@ -55,11 +68,8 @@ const SignupForm = () => {
             <label htmlFor="lastName">Last Name</label>
             <input
                 id="lastName"
-                name="lastName"
                 type="text"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.lastName}
+                {...formik.getFieldProps('lastName')}
             />
             {formik.touched.lastName && formik.errors.lastName ? (
                 <div>{formik.errors.lastName}</div>
@@ -67,11 +77,8 @@ const SignupForm = () => {
             <label htmlFor="email">Email Address</label>
             <input
                 id="email"
-                name="email"
                 type="email"
-                onChange={formik.handleChange}
-                onBlur={formik.handleBlur}
-                value={formik.values.email}
+                {...formik.getFieldProps('email')}
             />
             {formik.touched.email && formik.errors.email ? ( 
                 <div>{formik.errors.email}</div>
