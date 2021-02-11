8851e80ec61ce92017f1bc892cf9467d8210240f
Cory Spicer <corys4139@gmail.com>
2021-02-11 15:27:15 -0500

    formik custom field components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { useFormik } from 'formik';
+import { 
+    ErrorMessage, 
+    Formik, 
+    Form, 
+    Field, 
+    useFormik,
+    useField 
+    } from 'formik';
 import * as Yup from 'yup'
 import './styles.css';
 
@@ -28,63 +35,123 @@ import './styles.css';
 //     return errors
 // }
 
+const MyTextInput = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input className="text-input" {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    );
+};
+
+const MyCheckBox = ({ children, ...props }) => {
+    const [field, meta] = useField({ ...props, type: 'checkbox' });
+    return (
+        <div>
+            <label className="checkbox-input">
+                <input type="checkbox" {...field} {...props} />
+                {children}
+            </label>
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </div>
+    );
+};
+
+const MySelect = ({ label, ...props }) => {
+    const [field, meta] = useField(props);
+    return (
+        <div>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <select {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </div>
+    )
+}
+
 const SignupForm = () => {
-    const formik = useFormik({
-        initialValues: { 
-            email: "",
-            firstName: "",
-            lastName: ""
-        },
-
-        // validation using Yup
-
-        validationSchema: Yup.object({
-            firstName: Yup.string()
-                .max(15, 'Must be 15 characters or less')
-                .required('Required'),
-            lastName: Yup.string() 
-                .max(20, 'Must be 20 characters or less')
-                .required('Required'),
-            email: Yup.string()
-                .email('Invalid email address')
-                .required('Required'),
-        }),
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
-        }
-    });
     return ( 
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="firstName">First Name</label>
-            <input
-                id="firstName"
+        <>
+        <h1>Subscribe!</h1>
+        <Formik
+            initialValues={{ 
+                firstName: "", 
+                lastName: "", 
+                email: "",
+                acceptedTerm: false,
+                jobType: ""}}
+            validationSchema={Yup.object({
+                firstName: Yup.string()
+                    .max(15, 'Must be 15 characters or less')
+                    .required('Required'),
+                lastName: Yup.string() 
+                    .max(20, 'Must be 20 characters or less')
+                    .required('Required'),
+                email: Yup.string()
+                    .email('Invalid email address')
+                    .required('Required'),
+                acceptedTerms: Yup.boolean()
+                    .required('Required')
+                    .oneOf([true], 'You must accept the terms and conditions'),
+                jobType: Yup.string()
+                    .oneOf(
+                        ['designer', 'development', 'product', 'other'],
+                        'Invalid job type'
+                    )
+                    .required('Required')
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400)
+            }}
+        >
+        <Form>
+            <MyTextInput 
+                label="First Name"
+                name="firstName"
                 type="text"
-                {...formik.getFieldProps('firstName')}
+                placeholder="Cory"
             />
-            {formik.touched.firstName && formik.errors.firstName ? ( 
-                <div>{formik.errors.firstName}</div>
-            ) : null
-            }
-            <label htmlFor="lastName">Last Name</label>
-            <input
-                id="lastName"
+
+            <MyTextInput 
+                label="Last Name"
+                name="lastName"
                 type="text"
-                {...formik.getFieldProps('lastName')}
+                placeholder="Doe"
             />
-            {formik.touched.lastName && formik.errors.lastName ? (
-                <div>{formik.errors.lastName}</div>
-            ) : null}
-            <label htmlFor="email">Email Address</label>
-            <input
-                id="email"
+
+            <MyTextInput
+                label="Email Address"
+                name="email"
                 type="email"
-                {...formik.getFieldProps('email')}
+                placeholder="cory@cory.com"
             />
-            {formik.touched.email && formik.errors.email ? ( 
-                <div>{formik.errors.email}</div>
-            ) : null}
+
+            <MySelect label="Job Type" name="jobType">
+                <option value="">Select a job type</option>
+                <option value="designer">Designer</option>
+                <option value="development">Developer</option>
+                <option value="product">Product Manager</option>
+                <option value="other">Other</option>
+            </MySelect>
+
+            <MyCheckBox name="acceptedTerms">
+                I accept the terms and conditions.
+            </MyCheckBox>
+
             <button type="submit">Submit</button>
-        </form>
+        </Form>
+        </Formik>
+        </>
      );
 };
 
